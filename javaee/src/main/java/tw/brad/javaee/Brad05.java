aacfc19b4ab93480e70cd87caa85211701d45888
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-07-05 15:41:50 +0800

    07051541

--- javaee/src/main/java/tw/brad/javaee/Brad05.java
+++ javaee/src/main/java/tw/brad/javaee/Brad05.java
@@ -9,16 +9,26 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Brad05")
 public class Brad05 extends HttpServlet {
+	private String[] favoritiesName = {"打電腦","打遊戲","打籃球",
+			"打麻將","打毛線","打小孩"};
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 
 		String account = request.getParameter("account");
 		String passwd = request.getParameter("passwd");
 		String gender = request.getParameter("gender");
+		String[] favorities = request.getParameterValues("favority");
 		
 		System.out.println("account = " + account);
 		System.out.println("passwd = " + passwd);
 		System.out.println("gender = " + gender);
+		
+		System.out.println("Favorities:");
+		for (String favority : favorities) {
+			System.out.println(favoritiesName[Integer.parseInt(favority)]);
+		}
+		
+		System.out.println("----");
 		
 	}
 }
