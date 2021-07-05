a4147e365cbfd5e008c32aa0ebb873989fe5588e
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-07-05 14:27:35 +0800

    07051427

--- javaee/src/main/java/tw/brad/javaee/Brad04.java
+++ javaee/src/main/java/tw/brad/javaee/Brad04.java
@@ -16,14 +16,6 @@ public class Brad04 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
-		// 老師不好意思，可以再用一次透過如何開啟網頁嗎
-		// Port 8080 required by apache-tomcat-8.0.36 at localhost is already in use. 
-		// The server may already be running in another process, 
-		// or a system process may be using the port. 
-		// To start this server you will need to stop the other process 
-		// or change the port number(s).
-		// 老師，想問一下 這門課的 git 檔案網址
-		
 		Map<String,String[]> maps = request.getParameterMap();
 		Set<String> keys = maps.keySet();
 		for (String key : keys) {
