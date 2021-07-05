27537342ddc22bbdb9418d2b7f3a23e697389cc2
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-07-05 14:55:09 +0800

    07051455

---
+++ javaee/src/main/java/tw/brad/javaee/Brad05.java
@@ -0,0 +1,22 @@
+package tw.brad.javaee;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad05")
+public class Brad05 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String account = request.getParameter("account");
+		String passwd = request.getParameter("passwd");
+		
+		System.out.println(account);
+		System.out.println(passwd);
+		
+	}
+}
